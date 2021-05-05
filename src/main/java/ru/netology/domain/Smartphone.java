5330baaf7434df857f67a8f447a134f4e76d5fed
Diana Krapivina <mr.ikoma@mail.ru>
2021-05-05 18:26:41 +0300

    Add Book and Smartphone

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,4 +1,36 @@
 package ru.netology.domain;
 
-public class Smartphone {
+import java.util.Objects;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), manufacturer);
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
 }
