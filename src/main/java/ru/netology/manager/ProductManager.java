8baf866b2d44e2c1f81eeacc175542ab6e7e9e87
Diana Krapivina <mr.ikoma@mail.ru>
2021-05-11 23:36:17 +0300

    Add tests

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -19,12 +19,12 @@ public class ProductManager {
         repository.save(product);
     }
 
-    public Product[] searchyBy(String text) {
+    public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product: repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
-                // используйте System.arraycopy, чтобы скопировать всё из result в tmp
+                System.arraycopy(result,0,tmp,0,result.length);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
             }
--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -15,7 +15,9 @@ public class ProductManagerTest {
     private Book book1 = new Book(1, "Java", 1500, "Bates");
     private Book book2 = new Book(2, "C++", 1300, "Sierra");
     private Smartphone smartphone1 = new Smartphone(3, "Xiaomi redmi 4", 10_990, "Xiaomi");
-    private Smartphone smartphone2 = new Smartphone(4, "iPhone 12 mini", 79_990, "Apple");
+    private Smartphone smartphone2 = new Smartphone(4, "iPhone 12 mini", 69_990, "Apple");
+    private Smartphone smartphone3 = new Smartphone(5,"iPhone 12",79_990,"Apple");
+    private Smartphone smartphone4 = new Smartphone(6,"iPhone 12 Pro",99_990,"Apple");
 
     @BeforeEach
     public void setUp() {
@@ -23,40 +25,58 @@ public class ProductManagerTest {
         manager.add(book2);
         manager.add(smartphone1);
         manager.add(smartphone2);
+        manager.add(smartphone3);
+        manager.add(smartphone4);
     }
 
     @Test
     public void searchByBookName() {
-        Product[] actual = manager.searchyBy("Java");
+        Product[] actual = manager.searchBy("Java");
         Product[] expected = new Product[] {book1};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void searchByBookAuthor() {
-        Product[] actual = manager.searchyBy("C++");
+        Product[] actual = manager.searchBy("Sierra");
         Product[] expected = new Product[] {book2};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void searchBySmartphoneName() {
-        Product[] actual = manager.searchyBy("Xiaomi redmi 4");
+        Product[] actual = manager.searchBy("Xiaomi redmi 4");
         Product[] expected = new Product[] {smartphone1};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void searchBySmartphoneManufacturer() {
-        Product[] actual = manager.searchyBy("Apple");
-        Product[] expected = new Product[] {smartphone2};
+        Product[] actual = manager.searchBy("Apple");
+        Product[] expected = new Product[] {smartphone2,smartphone3,smartphone4};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void searchNonExistedProduct() {
-        Product[] actual = manager.searchyBy("Sony");
-        Product[] expected = new Product[0];
+        Product[] actual = manager.searchBy("Sony");
+        Product[] expected = new Product[]{};
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void searchByEmptyQuery() {
+        Product[] actual = manager.searchBy(null);
+        Product[] expected = new Product[]{};
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void removeById() {
+        repository.removeById(4);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{book1,book2,smartphone1,smartphone3,smartphone4};
+        assertArrayEquals(expected,actual);
+    }
+
 }
